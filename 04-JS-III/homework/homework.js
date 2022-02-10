a8b432a40718d529a87da5d2b3aad5e1871c9900
PunkCactus <78229011+PunkCactus@users.noreply.github.com>
2022-02-09 21:24:51 -0300

    Agrega JS-III parcialmente resuelto

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -81,7 +81,7 @@ function agregarNumeros(numeros) {
   // Suma todos los enteros y devuelve el valor
   // Tu código:
   let suma= 0;
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < numeros.length; i++) {
     suma= suma + numeros[i];
   }
   return suma;
@@ -93,7 +93,7 @@ function promedioResultadosTest(resultadosTest) {
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
   let suma=0;
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < resultadosTest.length; i++) {
     suma= suma + resultadosTest[i];  
   }
   return suma/resultadosTest.length;
@@ -104,8 +104,8 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
-  var max= numero[0];
-  for (let i = 0; i < array.length; i++) {
+  var max= numeros[0];
+  for (let i = 0; i < numeros.length; i++) {
     if(numeros[i]> max) max= numeros[i];
   }
   return max;
@@ -116,16 +116,23 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-
-
-  
+  let multiplicacion= 1;
+  if(arguments.length<1) return 0;
+  for (let i = 0; i < arguments.length; i++) {
+     multiplicacion= multiplicacion*arguments[i]; 
+  }
+  return multiplicacion;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  let contador=0;
+  for (let i = 0; i < arreglo.length; i++) {
+    if(arreglo[i]>19) contador++;
+  }
+  return contador++;
 }
 
 
@@ -134,7 +141,8 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+      if(numeroDeDia===1 || numeroDeDia===7) return 'Es fin de semana';
+      return 'Es dia Laboral';
 } 
 
 
@@ -142,6 +150,9 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  let numero= n.toString();
+  if(numero.charAt(0) === '9') return true;
+  else return false;
   
 }
 
@@ -150,7 +161,10 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+    for (let i = 0; i < arreglo.length-1; i++) {
+      if(arreglo[i]!== arreglo[i+1]) return false;
+    }
+    return true;
 } 
 
 
@@ -159,6 +173,13 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  let nuevoArray=[];
+  for (let i = 0; i < array.length; i++) {
+    if(array[i]==='Enero' || array[i]==='Marzo' || array[i]==='Noviembre')
+    nuevoArray.push(array[i]);
+    if(nuevoArray.length===3) return nuevoArray;
+  }
+  return 'No se encontraron los meses pedidos'
 }
 
 
@@ -166,6 +187,11 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  let arraynuevo=[];
+  for (let i = 0; i < array.length; i++) {
+    if(array[i]>100) arraynuevo.push(array[i]) 
+  }
+  return arraynuevo;
 }
 
 
@@ -177,6 +203,16 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  let array=[];
+  let suma= numero;
+  for (let i = 0; i < 10; i++) {
+    suma= suma+2;
+    if(suma===i) break;
+    else array.push(suma);
+  }
+  if(i<10) return 'Se interrumpió la ejecución'
+  else return array;
+
 }
 
 
